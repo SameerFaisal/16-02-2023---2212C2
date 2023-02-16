e525753e1b9b97a93a0d9db562f5debb5c5aaeb2
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-02-16 17:16:36 +0500

    16-02-2023

---
+++ 16-02-2023/app.js
@@ -0,0 +1,35 @@
+// alert("Shaheer Musani")
+
+// document.write("Shaheer")
+
+// console.log("shaheer")
+
+// document.write("Shaheer <br>")
+
+// document.write("Ahtesham")
+
+// document.write("<h1>Mubeen</h1>")
+// document.write("Mubeen")
+
+// var age=15
+// let stdName="John"
+
+
+
+// document.write(age)
+// document.write(stdName)
+
+let stdName="John"
+
+
+//Variable naming rules
+
+//1. Cannot contain spaces
+//2. Cannot start with digits
+//3. Cannot contain punctuation marks (. , ? !)
+//4. Cannot contain special characters except underscore (_)
+//5. Cannot be a javascript keyword
+
+
+
+
